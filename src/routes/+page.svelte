1b9f913c37d900ef871e827c5aeb0a3a0c97126b
Keeghan McGarry <keeghan_m@live.com>
2022-09-22 14:30:51 +0100

    Compares to Lichess Study (no variations)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,108 @@
 <script>
-	// 1. e4 c5 2. Nf3 e6 3. d4 cxd4 4. Nxd4 a6 5. Nc3 Qc7 6. Bd3 Bc5 7. Nb3 Ba7 8. O-O Nf6
+	// 1D1HCJEb
 	import { parse } from '@mliebelt/pgn-parser'
 
 	let username = ''
 	let message = ''
-	let games = []
 	let blackSide = false
-	let openingPgn = ''
-	let minMoves = 3
+	let studyId = ''
+	let minMoves = 1
 
 	async function getGames() {
 		if (username.length < 3) return
-		let url = `https://lichess.org/api/games/user/${username}?max=500&moves=true&tags=true&perfType=blitz,rapid,classical,correspondence`
+
+		let games = await fetchGames()
+		if (!games) return
+
+		let repertoire = await fetchRepertoire()
+		if (!repertoire) return
+
+		let compareSide = blackSide ? 'Black' : 'White'
+		let stats = []
+
+		for (let game of games) {
+			if (game.playerSide == compareSide) {
+				// First ensure the game matches at least the minimum moves in one
+				// of the lines in the repertoire
+				let match = true
+				// console.log({ 'New Game': game[0].tags.Site })
+				for (let chapter of repertoire) {
+					// console.log('New Chapter')
+					for (let i = 0; i <= minMoves * 2; i++) {
+						let gameMove = game[0]?.moves[i]?.notation?.notation
+						let repertoireMove = chapter[0]?.moves[i]?.notation?.notation
+
+						match = gameMove == repertoireMove
+						// console.log({ gameMove, repertoireMove, match })
+					}
+				}
+				// console.log({ match })
+
+				if (!match) continue // Move to the next game as this one isn't valid
+
+				let moveNumber = 0
+				let repertoireMove = ''
+				let gameMove = ''
+				let who = ''
+
+				for (let chapter of repertoire) {
+					for (let i = 0; i < Math.min(chapter[0].moves.length, game[0].moves.length); i++) {
+						let currentGameMove = game[0].moves[i].notation.notation
+						let modelMove = chapter[0].moves[i].notation.notation
+						let whosMove = game[0].moves[i].moveNumber == null ? 'Black' : 'White'
+						let currentMoveNumber = Math.ceil(i / 2)
+						if (currentGameMove != modelMove) {
+							if (currentMoveNumber > moveNumber) {
+								moveNumber = currentMoveNumber
+								repertoireMove = modelMove
+								gameMove = currentGameMove
+								who = whosMove == game.playerSide ? 'Player' : 'Opponent'
+							}
+							break
+						}
+					}
+				}
+				stats.push({
+					moveNumber,
+					repertoireMove,
+					gameMove,
+					who,
+					game
+				})
+			}
+		}
+		console.log(stats)
+	}
+
+	async function fetchRepertoire() {
+		let url = `https://lichess.org/api/study/${studyId}.pgn?comments=false&clocks=false`
+
+		const response = await fetch(url)
+		if (response.status == 404) {
+			message = 'Study ID'
+			return
+		}
+		const reader = response.body.pipeThrough(new TextDecoderStream()).getReader()
+		let repertoire = ''
+
+		while (true) {
+			message = 'Loading repertoire...'
+			const { value, done } = await reader.read()
+			if (done) break
+			repertoire += value
+		}
+
+		message = ''
+
+		let lines = repertoire.split('\n\n\n')
+		lines.pop()
+		lines = lines.map((x) => parse(x))
+
+		return lines
+	}
+
+	async function fetchGames() {
+		let url = `https://lichess.org/api/games/user/${username}?max=50&moves=true&tags=true&perfType=blitz,rapid,classical,correspondence`
 
 		const response = await fetch(url)
 		if (response.status == 404) {
@@ -27,8 +118,9 @@
 			if (done) break
 			pgn += value
 		}
+
 		message = ''
-		games = pgn.split('\n\n\n')
+		let games = pgn.split('\n\n\n')
 		games.pop()
 		games = games.map((x) => {
 			let game = parse(x)
@@ -37,40 +129,7 @@
 			return game
 		})
 
-		let parsedOpening = parse(openingPgn)
-		console.log(parsedOpening)
-		console.log(games)
-
-		let compareSide = blackSide ? 'Black' : 'White'
-		let stats = []
-		for (let game of games) {
-			if (game.playerSide == compareSide) {
-				// First ensure the minimum moves match
-				let match = true
-				for (let i = 0; i < minMoves * 2; i++) {
-					match =
-						game[0]?.moves[i]?.notation?.notation == parsedOpening[0]?.moves[i]?.notation?.notation
-				}
-				if (!match) continue // Move to the next game as this one isn't valid
-
-				stats.gameCount++
-				for (let i = 0; i < parsedOpening[0].moves.length; i++) {
-					let gameMove = game[0].moves[i].notation.notation
-					let modelMove = parsedOpening[0].moves[i].notation.notation
-					let whosMove = game[0].moves[i].moveNumber == null ? 'Black' : 'White'
-					if (gameMove != modelMove) {
-						stats.push({
-							moveNumber: Math.ceil(i / 2),
-							who: whosMove == game.playerSide ? 'Player' : 'Opponent',
-							should: modelMove,
-							did: gameMove
-						})
-						break
-					}
-				}
-			}
-		}
-		console.log(stats)
+		return games
 	}
 </script>
 
@@ -92,14 +151,14 @@
 			/>
 		</div>
 		<div class="-space-between flex flex-row items-center gap-4">
-			<label for="pgnCompare" class="font-bold">Opening PGN</label>
+			<label for="pgnCompare" class="font-bold">LiChess Study Id</label>
 			<input
 				required
 				type="text"
 				name="pgnCompare"
 				id="pgnCompare"
 				class="border-2 border-slate-800 px-4 py-2"
-				bind:value={openingPgn}
+				bind:value={studyId}
 			/>
 		</div>
 		<div class="flex flex-row content-center gap-2 align-middle font-bold">
