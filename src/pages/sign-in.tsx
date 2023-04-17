426fbe0a4ec6bdc88eac17d4374f5200a6e68088
Keeghan McGarry <keeghan_m@live.com>
2023-04-17 20:47:54 +0100

    Clerk Login

---
+++ src/pages/sign-in.tsx
@@ -0,0 +1,22 @@
+import { SignIn } from "@clerk/nextjs";
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Login: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>
+          Openings Deviations - Check how well you know your Chess Openings
+        </title>
+        <meta
+          name="description"
+          content="Improve your chess game with Opening Deviations! Our website analyzes your chess games and compares them to your opening repertoire to identify where you're going wrong. Get personalized advice on how to improve your openings and take your game to the next level."
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex justify-center items-center"><SignIn /></main>
+      </>
+  )}
+
+  export default Login
