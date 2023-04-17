fb72339cbf86cda48649b350d384a7cbe3ea5478
Keeghan McGarry <keeghan_m@live.com>
2023-04-17 19:38:16 +0100

    Initial Next Setup

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./node_modules/flowbite-react/**/*.js","./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+} satisfies Config;
